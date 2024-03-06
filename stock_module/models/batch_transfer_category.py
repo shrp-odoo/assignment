3d92d553f0b8756ab75bb0f336e4e116a0a0d2bd
shrp-odoo <shrp@odoo.com>
2024-03-06 12:27:04 +0530

    [WIP] tasks

--- stock_module/models/batch_transfer_category.py
+++ stock_module/models/batch_transfer_category.py
@@ -5,12 +5,12 @@ from odoo import models, fields, api
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    volume = fields.Float(string="Volume", compute="_compute_volume")
+    volume = fields.Float(string="Volume", compute="_compute_volume", default=0)
 
-    @api.depends("product_id")
+    @api.depends("move_line_ids.product_id", "move_line_ids.product_id.volume")
     def _compute_volume(self):
-        v = 0
-        for move_line in self.move_line_ids:
-            v += move_line.product_id.volume
-
-        self.volume = v
+        for picking in self:
+            picking.volume = 0 
+            for move_line in picking.move_line_ids:
+                picking.volume += move_line.product_id.volume
+                
